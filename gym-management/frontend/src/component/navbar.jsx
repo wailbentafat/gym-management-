f529fe47e26051bc830167300a63464dd7ce5d4e
wailsb <wail.saribey@gmail.com>
2024-10-09 16:50:28 +0100

    ani bdlt tailwind

--- gym-management/frontend/src/component/navbar.jsx
+++ gym-management/frontend/src/component/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar({ title = "Navbar" }) {
     return (
-        <div className="bg-gray-800 text-white p-4">
+        <div className="bg-beigecustom text-white p-4">
             <h1 className="text-lg font-bold">{title}</h1>
         </div>
     );
