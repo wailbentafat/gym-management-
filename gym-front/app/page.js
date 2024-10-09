0b4d10183b703d0833b379b0af12017f94a2df31
wailsb <wail.saribey@gmail.com>
2024-10-09 09:59:02 +0100

    n7it les errors

---
+++ gym-front/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import header from "@/component/header"
+import sidebar from "@/component/sidebar"
+export default function Home() {
+  return (
+    <div>
+        <header/>
+        <sidebar/>
+    </div>
+  );
+}
