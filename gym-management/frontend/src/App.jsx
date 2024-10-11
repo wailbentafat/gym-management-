7cc402da1ac9fb80abcf875d7e979c72b6580ed3
wailsb <wail.saribey@gmail.com>
2024-10-11 10:33:27 +0100

    drt reg form

--- gym-management/frontend/src/App.jsx
+++ gym-management/frontend/src/App.jsx
@@ -4,12 +4,12 @@ import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 import Outlet from './component/outlet';
 import Sidebar from './component/sidebar';
-import Registartionpage from './pages/registration';
+
 function App() {
   
     return (
         <div id="App" className='h-full'>
-            <Registartionpage/>
+          <Outlet/>
         </div>
     )
 }
--- gym-management/frontend/src/component/outlet.jsx
+++ gym-management/frontend/src/component/outlet.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "./sidebar";
 import Navbar from "./navbar";
+import RegistrForm from "./registration/registrform";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Outlet() {
@@ -12,14 +13,14 @@ export default function Outlet() {
                 <Routes>
                   <Route path="/" element={<>dashboard</>}/>
                   <Route path="/admin" element={<>admin</>} />
-                  <Route path="/reg" element={<>registration</>} />
+                  <Route path="/reg" element={<RegistrForm/>} />
                   <Route path="/plan" element={<>plan</>} />
                   <Route path="/payment" element={<>payment</>} />
                   <Route path="/inv" element={<>inventory</>} />
                   <Route path="/members" element={<>members</>} />
                   <Route path="/report" element={<>report</>} />
                   <Route path="*" element={<>404</>} />
-                  <Route path ="/test" element={children} />
+                  <Route path ="/test" element={<>hna cmp</>} />
                 </Routes>
             </div>
           </div>
