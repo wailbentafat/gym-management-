1d8843bcd4c9c30989e4032fa0f0638ec09c0b21
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2024-10-12 00:22:32 +0100

    ychkopi t93rt

--- gym-management/frontend/src/pages/registration.jsx
+++ gym-management/frontend/src/pages/registration.jsx
@@ -5,7 +5,7 @@ export default function Registartionpage() {
     return(
         <div className="h-full flex flex-col grow[3] h-full  border-white bg-beigecustom">
                 <div className=" border-white h-1/4 bg-beigecustom ml-5 mt-12 ">
-                <h1>Become member !</h1>
+                <h1 className="">Become member !</h1>
                 <h2>Register now</h2>
                 </div>
                 <div className=" rounded-full   h-3/4 ml-5 mr-10">
