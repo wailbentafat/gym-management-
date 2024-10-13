ef99ac82ba45b629e4cc3bf4654261ddd9c3360a
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2024-10-13 16:55:23 +0100

    fk

--- gym-management/frontend/src/pages/plan.jsx
+++ gym-management/frontend/src/pages/plan.jsx
@@ -5,9 +5,11 @@ import Addplan from "../component/plans/addplan";
 export default function Planpage(){
     return(
         <div className="bg-beigecustom ">
-            <button>add plan</button>
+            <button className="bg-white rounded-full shadow-lg ml-9 px-7 py-1 font-bold  text-xl mb-5">add plan</button>
             <Addplan/>
+            <div className="mt-4 ml-9">
             <Plan/>
+            </div>
         </div>
 
     )
